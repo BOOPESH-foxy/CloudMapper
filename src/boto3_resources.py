ae946fdb68ac638477874b54e217335f129b34a6
boopesh-foxy <boopesh.mc@gmail.com>
2025-10-29 15:18:42 +0530

    data model init

--- src/boto3_resources.py
+++ src/boto3_resources.py
@@ -1,4 +1,19 @@
 import boto3
+from botocore.config import Config
 
-def boto3_ec2_resource(region: str):
-    return boto3.resource('ec2',region)
+
+def boto3_session():
+    return boto3.session.Session
+
+def ec2_session_client(region: str):
+    session = boto3_session()
+    ec2 = session.client("ec2", region_name=region, config=Config(retries={"max_attempts": 10}))
+
+def elb_session(region: str):
+    session = boto3_session()
+    elb2 = session.client("elbv2", region_name=region, config=Config(retries={"max_attempts": 10}))
+    
+def ec2_session_resource(region: str):
+    session = boto3_session()
+    ec2_res = session.resource("ec2", region_name=region, config=Config(retries={"max_attempts": 10}))
+
--- src/get_services.py
+++ src/get_services.py
@@ -1,2 +1,27 @@
-def get_resources():
-    print("In get resources")
+import boto3
+import boto3_resources
+from botocore.exceptions import ClientError
+from typing import Any
+
+def fetch_resources():
+    pass
+
+class data_init:
+
+    def __init__(self):
+
+        self.albs: dict[str, dict[str, Any]] = {}
+        self.vpcs: dict[str, dict[str, Any]] = {}
+        self.subnets: dict[str, dict[str, Any]] = {}
+        self.route_tables: dict[str, dict[str, Any]] = {}
+        self.igws: dict[str, dict[str, Any]] = {}
+        self.nat_gws: dict[str, dict[str, Any]] = {}
+        self.vpc_endpoints: dict[str, dict[str, Any]] = {}
+        self.peering: dict[str, dict[str, Any]] = {}
+        self.vgws: dict[str, dict[str, Any]] = {}
+        self.sg: dict[str, dict[str, Any]] = {}
+        self.enis: dict[str, dict[str, Any]] = {}
+        self.instances: dict[str, dict[str, Any]] = {}
+        self.albs: dict[str, dict[str, Any]] = {}
+        self.nlbs: dict[str, dict[str, Any]] = {}
+        self.target_groups: dict[str, dict[str, Any]] = {}

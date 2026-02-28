443fbad7d4e16cb92c1253f347d18f900250fde1
BOOPESH-foxy <boopesh.mc@gmail.com>
2026-02-28 16:29:19 +0530

    graph server update

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine:3.18 AS builder
+
+# install graphviz ()
+RUN apk add --no-cache graphviz
+
+WORKDIR /app
+COPY graph.dot .
+
+RUN dot -Tsvg graph.dot -o account-map.svg
+
+FROM nginx:alpine
+
+COPY --from=builder /app/map.svg /usr/share/nginx/html/index.html
+EXPOSE 80
--- mapper/boto3_resources.py
+++ mapper/boto3_resources.py
@@ -3,7 +3,7 @@ from botocore.config import Config
 
 
 def boto3_session():
-    return boto3.session.Session
+    return boto3.session.Session()
 
 def ec2_session_client(region: str):
     return boto3.client('ec2',region_name=region,config=Config(retries={"max_attempts": 10}))
@@ -11,8 +11,9 @@ def ec2_session_client(region: str):
 def elb_session(region: str):
     session = boto3_session()
     elb2 = session.client("elbv2", region_name=region, config=Config(retries={"max_attempts": 10}))
-    
+    return elbv2
+
 def ec2_session_resource(region: str):
     session = boto3_session()
     ec2_res = session.resource("ec2", region_name=region, config=Config(retries={"max_attempts": 10}))
-
+    return ec2_res

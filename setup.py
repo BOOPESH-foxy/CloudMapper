27329f25a48e7d1656e821707eff6b705f27ceae
BOOPESH-foxy <boopesh.mc@gmail.com>
2026-03-07 12:56:04 +0530

    Feature: Added Dev containers

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aws-cloud-mapper",
+    version="0.1.0",
+    description="AWS Cloud Mapper - Network visualization tool",
+    author="boopesh-foxy",
+    packages=find_packages(),
+    install_requires=[
+        "boto3>=1.28.0",
+        "botocore>=1.31.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cloud-mapper=mapper.main:main",
+        ],
+    },
+    python_requires=">=3.11",
+)
